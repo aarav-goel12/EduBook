f2eaf0d61acb4961fc3f8578293b4006ff06ff51
aarav <aarav.goel109@gmail.com>
2024-05-28 16:17:58 +0530

    fragmentcontainerview added, Welcome Fragment created, navGraph updated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,8 @@ android {
 
 dependencies {
 
+    implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
+    implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
