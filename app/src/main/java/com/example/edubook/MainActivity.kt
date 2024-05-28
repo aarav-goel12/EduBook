a0ff628f5a8a4aa343cb094c6ab629f85719ca51
aarav <aarav.goel109@gmail.com>
2024-05-28 15:54:34 +0530

    a little clean up has been done

---
+++ app/src/main/java/com/example/edubook/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.edubook
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.navigateUp
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.example.edubook.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+}
