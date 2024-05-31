afdf5e3898dd4278efa141b551de0ab8d257b345
aarav <aarav.goel109@gmail.com>
2024-05-31 16:25:05 +0530

    Navigation failed, Nav controller added, inflated

--- app/src/main/java/com/example/edubook/WelcomeFragment.kt
+++ app/src/main/java/com/example/edubook/WelcomeFragment.kt
@@ -5,12 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class WelcomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -20,9 +18,14 @@ class WelcomeFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+//        inflation: taking views from xml and it inflates them into a view hierarchy that
+//        holds objects for each view that we have defined in the xml
+//        textview---->inflate()---> View --> object of TextView
+        val view = inflater.inflate(R.layout.fragment_welcome, container, false)
+        val proceedButton = view.findViewById<Button>(R.id.ProceedButtonId)
+        proceedButton.setOnClickListener {
+            view.findNavController().navigate(R.id.action_welcomeFragment_to_subjectListFragment)
+        }
+        return view
     }
-
-
-            }
+}
