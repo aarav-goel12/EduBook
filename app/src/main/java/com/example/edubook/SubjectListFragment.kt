5a551eec793ba771bf16d2f731c6b41a2c2d4d8b
Aarav <aarav.goel109@gmail.com>
2024-06-04 17:09:13 +0530

    Navigation in subjectLIstFragment added

--- app/src/main/java/com/example/edubook/SubjectListFragment.kt
+++ app/src/main/java/com/example/edubook/SubjectListFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.findNavController
 
 class SubjectListFragment : Fragment() {
 
@@ -18,7 +20,23 @@ class SubjectListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_subject_list, container, false)
+        val view =  inflater.inflate(R.layout.fragment_subject_list, container, false)
+        val mathButton = view.findViewById<Button>(R.id.MathId)
+    mathButton.setOnClickListener {
+        view.findNavController().navigate(R.id.action_subjectListFragment_to_mathFragment)
+    }
+    val physicsButton = view.findViewById<Button>(R.id.PhysicsId)
+    physicsButton.setOnClickListener {
+        view.findNavController().navigate(R.id.action_subjectListFragment_to_physicsFragment)
+    }
+    val chemistryButton = view.findViewById<Button>(R.id.ChemistryId)
+    chemistryButton.setOnClickListener {
+        view.findNavController().navigate(R.id.action_subjectListFragment_to_chemistryFragment)
+    }
+    val cSButton = view.findViewById<Button>(R.id.CSId)
+    chemistryButton.setOnClickListener {
+        view.findNavController().navigate(R.id.action_subjectListFragment_to_computerScienceFragment)
+    }
+    return view
     }
 }
