c4dd87939a5f48ad065c0e516c5ebfc8da00360e
Aarav <aarav.goel109@gmail.com>
2024-06-11 16:36:09 +0530

    duplicatclass error, safeArgs added, Direction and Args used(not running)

--- app/src/main/java/com/example/edubook/TestFragment.kt
+++ app/src/main/java/com/example/edubook/TestFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+
 class TestFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,6 +17,10 @@ class TestFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         val view = inflater.inflate(R.layout.fragment_test, container, false)
+        val text = view.findViewById<TextView>(R.id.TextTestId)
+        val argument = TestFragmentArgs.fromBundle(requireArguments())
+        val message = argument.Test
+        text.setText(message)
         return view
     }
 }
