a5164f52eeb034791bab2d86c4a7583d438c6ae1
Aarav <aarav.goel109@gmail.com>
2024-06-14 15:57:09 +0530

    Technical Issues

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -50,7 +50,7 @@ dependencies {
 //    implementation(libs.androidx.constraintlayout)
 //    implementation(libs.androidx.navigation.fragment.ktx)
 //    implementation(libs.androidx.navigation.ui.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
+//    testImplementation(libs.junit)
+//    androidTestImplementation(libs.androidx.junit)
+//    androidTestImplementation(libs.androidx.espresso.core)
 }
